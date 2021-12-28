244a4d23d2d6cbd9887af54c48bf25465c63606c
wambugujoseph <kibew.joseph@gmail.com>
2021-12-28 13:28:19 +0300

    Conformation url

--- src/main/java/com/transsaction/mpesa/controller/TestApp.java
+++ src/main/java/com/transsaction/mpesa/controller/TestApp.java
@@ -1,15 +1,22 @@
 package com.transsaction.mpesa.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.transsaction.mpesa.model.Authorized;
 import com.transsaction.mpesa.model.ConsumerComponent;
 import com.transsaction.mpesa.service.HttpService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestApp {
+
+    public static Logger logger = LoggerFactory.getLogger(TestApp.class);
 
     @Autowired
     private ConsumerComponent consumerComponent;
@@ -31,6 +38,17 @@ public class TestApp {
             e.printStackTrace();
         }
         return new Authorized();
+    }
+
+    @RequestMapping(value="/comfirmation")
+    public void mpesaConfirmation(@RequestBody Object data){
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            logger.info(mapper.writeValueAsString(data));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
