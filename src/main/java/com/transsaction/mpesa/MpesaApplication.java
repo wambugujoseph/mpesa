04983172871a4c99d2a9ed9b40800e9f70847b3d
Joseph Kibe <joseph.kibe@switchlinkafrica.co.ke>
2023-01-18 09:46:39 +0300

    test change 2

--- src/main/java/com/transsaction/mpesa/MpesaApplication.java
+++ src/main/java/com/transsaction/mpesa/MpesaApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MpesaApplication {
 
-	/**
-	 * Project init
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MpesaApplication.class, args);
 	}
